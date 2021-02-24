229d072c45491923d6acbd720e18a1777293c75c
theoneandonlyirvin <budgebrian21@gmail.com>
2021-02-23 21:31:47 -0500

    Refactor + write tests to ensure turn switches on each move

--- test/game.h
+++ test/game.h
@@ -27,31 +27,43 @@ TEST_CASE("Index operators work", "[game]")
     REQUIRE(game(7, 7) == BlackRook);
 }
 
-TEST_CASE("Ensure players take turns and moves are counted", "[game")
+TEST_CASE("Ensure moves are counted correctly", "[game]")
 {
     Game game;
     Move info;
     bool valid = game.move("a2", "a3", info);
     REQUIRE(valid);
-    REQUIRE(info.captured == Empty);
-    REQUIRE(info.to == "a3");
-    REQUIRE(info.from == "a2");
     REQUIRE(game.half_moves() == 1);
     REQUIRE(game.move_num() == 1);
 
     valid = game.move("h7", "h6", info);
     REQUIRE(valid);
-    REQUIRE(info.captured == Empty);
-    REQUIRE(info.to == "h6");
-    REQUIRE(info.from == "h7");
     REQUIRE(game.half_moves() == 2);
     REQUIRE(game.move_num() == 2);
 
     valid = game.move("a3", "a4", info);
     REQUIRE(valid);
-    REQUIRE(info.captured == Empty);
-    REQUIRE(info.to == "a4");
-    REQUIRE(info.from == "a3");
     REQUIRE(game.half_moves() == 3);
     REQUIRE(game.move_num() == 2);
+}
+
+TEST_CASE("Ensure turn changes on each move", "[game]")
+{
+    Game game;
+    
+    REQUIRE(game.turn() == 0);
+
+    Move info;
+    bool valid = game.move("a2", "a3", info);
+    REQUIRE(valid);
+    REQUIRE(game.turn() == 1);
+    
+    valid = game.move("h7", "h6", info);
+    REQUIRE(valid);
+    REQUIRE(game.turn() == 0);
+    
+    valid = game.move("a3", "a4", info);
+    REQUIRE(valid);
+    REQUIRE(game.turn() == 1);
+    
 }
